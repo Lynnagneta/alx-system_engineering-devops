e41b41a7058a00a896a8ed66a82ddb100681b4bd
lynnagneta <lynnetteagneta@gmail.com>
2024-06-09 08:35:02 -0700

    api-advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+'''
+module for the Reddit API
+'''
+
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+def number_of_subscribers(subscribers):
+    '''function that queries and returns the number of subscribers '''
+    headers = {'User-agent': 'test'}
+    res = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subscribers), headers=headers)
+    try:
+        subs = res.json()['data']['subscribers']
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    return subs
