0d499ac2744d1b2ee4ff4ab36b741a3e584ad5cd
lynnagneta <101558545+Lynnagneta@users.noreply.github.com>
2025-01-15 23:17:21 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Creatring a file in /tmp
-file {'school':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# process that kills a process named killmenow
-
-exec { 'pkill -f killmenow':
-  path    => '/usr/bin/:usr/local/bin/:/bin/'
-}
